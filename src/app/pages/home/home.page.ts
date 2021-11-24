66358353f9fe6133901a382058ee43bfa5eb194f
Bryan Kahiga <bryanhiggz1@gmail.com>
2021-11-24 11:16:44 +0300

    Added render function

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,24 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
 import { CheckoutService } from 'src/app/services/Checkout/checkout.service';
 import { TokenService } from 'src/app/services/Token/token.service';
 
+declare const braintree;
+declare const client;
+declare const paypal;
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
+
 export class HomePage implements OnInit {
 
-  constructor(private tokenService:TokenService, private checkoutService:CheckoutService) { }
+
+  constructor(public loadingController: LoadingController,private tokenService: TokenService, private checkoutService: CheckoutService) { }
 
   ngOnInit() {
     this.getToken();
   }
 
-  getToken(){
-    this.tokenService.getToken().subscribe((token)=>{
-      console.log(token);
+  async getToken() {
+    const loading = await this.loadingController.create({
+      spinner: "circles",
+      message: 'Please wait...',
+    });
+    await loading.present();
+
+    this.tokenService.getToken().subscribe((token:any) => {
+      console.log(token.clientToken);
+
+      braintree.dropin.create({
+        authorization: token.clientToken,
+        container: document.getElementById('dropin-container'),
+      });
+      loading.dismiss();
     })
+
+
+
+
   }
 
 }
