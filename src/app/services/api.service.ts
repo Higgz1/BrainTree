bd18a6913160c4eb1c0d4078e517444ccb50db69
Bryan Kahiga <bryanhiggz1@gmail.com>
2021-11-24 09:03:06 +0300

    initialize api service

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  url: string = 'http://localhost:5001/braintree-functions/us-central1';
+
+  constructor(private http: HttpClient) {}
+
+  get(endpoint: string, params?: any, reqOpts?: any) {
+    if (!reqOpts) {
+      reqOpts = {
+        params: new HttpParams()
+      };
+    }
+
+    // Support easy query params for GET requests
+    if (params) {
+      reqOpts.params = new HttpParams();
+      for (let k in params) {
+        reqOpts.params = reqOpts.params.set(k, params[k]);
+      }
+    }
+    // const headers = new HttpHeaders().set('Authorization', 'Bearer ' + this.token);
+    return this.http.get(this.url + '/' + endpoint,{ params });
+  }
+}
